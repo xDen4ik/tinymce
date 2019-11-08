cee442ea52b6d0686361a77927e69d43c79a3031
Dylan Just <>
2019-11-08 22:14:33 +1000

    Port to fast-check and new assertions library.

--- modules/katamari/src/test/ts/atomic/api/option/OptionsTest.ts
+++ modules/katamari/src/test/ts/atomic/api/option/OptionsTest.ts
@@ -3,8 +3,8 @@ import * as Fun from 'ephox/katamari/api/Fun';
 import { Option } from 'ephox/katamari/api/Option';
 import * as Options from 'ephox/katamari/api/Options';
 import * as ArbDataTypes from 'ephox/katamari/test/arb/ArbDataTypes';
-import Jsc from '@ephox/wrap-jsverify';
-import { UnitTest, assert } from '@ephox/bedrock-client';
+import fc from 'fast-check';
+import { UnitTest, Assert } from '@ephox/bedrock-client';
 
 UnitTest.test('OptionsTest', function () {
   const person = function (name, age, address) {
@@ -12,110 +12,91 @@ UnitTest.test('OptionsTest', function () {
   };
 
   const arr1 = [Option.some(1), Option.none(), Option.some(2), Option.some(3), Option.none(), Option.none(), Option.none(), Option.none(), Option.some(4)];
-  assert.eq([1, 2, 3, 4], Options.cat(arr1));
+  Assert.eq('eq', [1, 2, 3, 4], Options.cat(arr1));
 
-  assert.eq(undefined, Option.some(1).each(Fun.identity), 'each returns undefined');
-  assert.eq(undefined, Option.none().each(Fun.identity), 'each returns undefined');
+  Assert.eq('each returns undefined 1', undefined, Option.some(1).each(Fun.identity));
+  Assert.eq('each returns undefined 2', undefined, Option.none().each(Fun.identity));
+});
 
-  Jsc.property(
-    'Options.cat of only nones should be an empty array',
-    Jsc.array(ArbDataTypes.optionNone),
+UnitTest.test('Options.cat of only nones should be an empty array', () => {
+  fc.assert(fc.property(
+    fc.array(ArbDataTypes.optionNone()),
     function (options) {
       const output = Options.cat(options);
-      return Jsc.eq(0, output.length);
+      Assert.eq('eq',  0, output.length);
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.cat of only somes should have the same length',
-    Jsc.array(ArbDataTypes.optionSome),
+UnitTest.test('Options.cat of only somes should have the same length', () => {
+  fc.assert(fc.property(
+    fc.array(ArbDataTypes.optionSome(fc.integer())),
     function (options) {
       const output = Options.cat(options);
-      return Jsc.eq(options.length, output.length);
+      Assert.eq('eq',  options.length, output.length);
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.cat of Arr.map(xs, Option.some) should be xs',
-    Jsc.array(Jsc.json),
+UnitTest.test('Options.cat of Arr.map(xs, Option.some) should be xs', () => {
+  fc.assert(fc.property(
+    fc.array(fc.json()),
     function (arr) {
       const options = Arr.map(arr, Option.some);
       const output = Options.cat(options);
-      return Jsc.eq(arr, output);
+      Assert.eq('eq',  arr, output);
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.cat of somes and nones should have length <= original',
-    Jsc.array(ArbDataTypes.option),
+UnitTest.test('Options.cat of somes and nones should have length <= original', () => {
+  fc.assert(fc.property(
+    fc.array(ArbDataTypes.option(fc.integer())),
     function (arr) {
       const output = Options.cat(arr);
-      return Jsc.eq(output.length <= arr.length, true);
+      Assert.eq('eq',  output.length <= arr.length, true);
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.cat of nones.concat(somes).concat(nones) should be somes',
-    Jsc.array(Jsc.json),
-    Jsc.array(Jsc.json),
-    Jsc.array(Jsc.json),
+UnitTest.test('Options.cat of nones.concat(somes).concat(nones) should be somes', () => {
+  fc.assert(fc.property(
+    fc.array(fc.json()),
+    fc.array(fc.json()),
+    fc.array(fc.json()),
     function (before, on, after) {
       const beforeNones = Arr.map(before, Option.none);
       const afterNones = Arr.map(after, Option.none);
       const onSomes = Arr.map(on, Option.some);
       const output = Options.cat(beforeNones.concat(onSomes).concat(afterNones));
-      return Jsc.eq(on, output);
+      Assert.eq('eq',  on, output);
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.findMap of empty is none',
-    Jsc.fun(ArbDataTypes.option),
+UnitTest.test('Options.findMap of empty is none', () => {
+  fc.assert(fc.property(
+    fc.func(ArbDataTypes.option(fc.integer())),
     function (f) {
-      return Jsc.eq(true, Options.findMap([ ], f).isNone());
+      Assert.eq('eq',  true, Options.findMap([ ], f).isNone());
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.findMap of non-empty is first if f is Option.some',
-    Jsc.nearray(Jsc.json),
+UnitTest.test('Options.findMap of non-empty is first if f is Option.some', () => {
+  fc.assert(fc.property(
+    fc.array(fc.json(), 1, 40),
     function (arr) {
-      return Jsc.eq(arr[0], Options.findMap(arr, Option.some).getOrDie());
+      Assert.eq('eq',  arr[0], Options.findMap(arr, Option.some).getOrDie());
     }
-  );
+  ));
+});
 
-  Jsc.property(
-    'Options.findMap of non-empty is none if f is Option.none',
-    Jsc.nearray(Jsc.json),
+UnitTest.test('Options.findMap of non-empty is none if f is Option.none', () => {
+  fc.assert(fc.property(
+    fc.array(fc.json(), 1, 40),
     function (arr) {
-      return Jsc.eq(true, Options.findMap(arr, Option.none).isNone());
+      Assert.eq('eq',  true, Options.findMap(arr, Option.none).isNone());
     }
-  );
-
-  Jsc.property(
-    'Options.findMap always returns an option',
-    Jsc.nearray(Jsc.json),
-    Jsc.fun(ArbDataTypes.option),
-    function (arr, f) {
-      const output = Options.findMap(arr, f);
-      return output.isNone() || output.isSome();
-    }
-  );
-
-  Jsc.property(
-    'Options.lift with two arguments mirrors cat with two items',
-    ArbDataTypes.option,
-    ArbDataTypes.option,
-    Jsc.fun(Jsc.json),
-    function (a, b, f) {
-      const args = Options.cat([a, b]);
-      const output = Options.lift2(a, b, f);
-
-      if (args.length === 2) {
-        return Jsc.eq(output.getOrDie(), f.apply(undefined, args));
-      } else {
-        return output.isNone();
-      }
-    }
-  );
+  ));
 });

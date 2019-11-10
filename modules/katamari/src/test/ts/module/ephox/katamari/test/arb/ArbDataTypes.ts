21a1c819f423a9bf2883ad8ff0eba417ef1649c7
Dylan Just <>
2019-11-10 12:08:55 +1000

    Arbitraries for Future use.

--- modules/katamari/src/test/ts/module/ephox/katamari/test/arb/ArbDataTypes.ts
+++ modules/katamari/src/test/ts/module/ephox/katamari/test/arb/ArbDataTypes.ts
@@ -1,6 +1,10 @@
 import { Option } from 'ephox/katamari/api/Option';
 import { Result } from 'ephox/katamari/api/Result';
-import fc, { Arbitrary } from 'fast-check';
+import { Future } from 'ephox/katamari/api/Future';
+import { setTimeout } from '@ephox/dom-globals';
+import fc from 'fast-check';
+
+type Arbitrary<A> = fc.Arbitrary<A>;
 
 export const arbResultError = <A, E> (arbE: Arbitrary<E>): Arbitrary<Result<A, E>> =>
   arbE.map(Result.error);
@@ -17,3 +21,16 @@ export const arbOptionSome = <T> (at: Arbitrary<T>) => at.map(Option.some);
 export const arbOption = <T> (at: Arbitrary<T>) => fc.oneof(arbOptionNone(), arbOptionSome(at));
 
 export const arbNegativeInteger = () => fc.integer(Number.MIN_SAFE_INTEGER, -1);
+
+export const arbFutureNow = <A> (arbA: Arbitrary<A>): Arbitrary<Future<A>> =>
+  arbA.map(Future.pure);
+
+export const arbFutureSoon = <A> (arbA: Arbitrary<A>): Arbitrary<Future<A>> =>
+  arbA.map((a) => Future.nu((cb) => {
+    setTimeout(() => {
+      cb(a);
+    }, 10);
+  }));
+
+export const arbFutureNever = <A> (): Arbitrary<Future<A>> =>
+  fc.constant(Future.nu(() => {}));

ec378447171b7f318c0f65b7db016f87e39d5a12
Millie Macdonald <millie.macdonald@tiny.cloud>
2020-06-15 14:44:39 +1000

    Code from demoing how to convert legacy tests - note: code is broken, branch is temporary

--- modules/tinymce/src/plugins/lists/test/ts/browser/BackspaceDeleteInlineTest.ts
+++ modules/tinymce/src/plugins/lists/test/ts/browser/BackspaceDeleteInlineTest.ts
@@ -1,72 +1,65 @@
-import { Pipeline, Log } from '@ephox/agar';
+import { Assertions, Log, Pipeline, Step } from '@ephox/agar';
+import { Element } from '@ephox/sugar';
 import { UnitTest } from '@ephox/bedrock-client';
 import { document } from '@ephox/dom-globals';
-import { LegacyUnit } from '@ephox/mcagar';
-import Editor from 'tinymce/core/api/Editor';
+import { TinyLoader } from '@ephox/mcagar';
 import DomQuery from 'tinymce/core/api/dom/DomQuery';
-import EditorManager from 'tinymce/core/api/EditorManager';
 import Plugin from 'tinymce/plugins/lists/Plugin';
 import SilverTheme from 'tinymce/themes/silver/Theme';
 
 UnitTest.asynctest('tinymce.lists.browser.BackspaceDeleteInlineTest', (success, failure) => {
-  const suite = LegacyUnit.createSuite<Editor>();
-
   Plugin();
   SilverTheme();
-
-  suite.test('TestCase-TBA: Lists: Backspace at beginning of LI on body UL', function (editor) {
-    editor.focus();
-    editor.selection.setCursorLocation(editor.getBody().firstChild.firstChild, 0);
-    editor.plugins.lists.backspaceDelete();
-    LegacyUnit.equal(DomQuery('#lists ul').length, 3);
-    LegacyUnit.equal(DomQuery('#lists li').length, 3);
-  });
-
-  suite.test('TestCase-TBA: Lists: Delete at end of LI on body UL', function (editor) {
-    editor.focus();
-    editor.selection.setCursorLocation(editor.getBody().firstChild.firstChild, 1);
-    editor.plugins.lists.backspaceDelete(true);
-    LegacyUnit.equal(DomQuery('#lists ul').length, 3);
-    LegacyUnit.equal(DomQuery('#lists li').length, 3);
-  });
 
   const teardown = function (editor, div) {
     editor.remove();
     div.parentNode.removeChild(div);
   };
 
-  const setup = function (success, failure) {
-    const div = document.createElement('div');
+  TinyLoader.setupFromElement((editor, onSuccess, onFailure) => {
+    // const div = document.createElement('div');
+    // div.innerHTML = (
+    //   '<div id="lists">' +
+    //   '<ul><li>before</li></ul>' +
+    //   '<ul id="inline"><li>x</li></ul>' +
+    //   '<ul><li>after</li></ul>' +
+    //   '</div>'
+    // );
+    // document.body.appendChild(div);
 
-    div.innerHTML = (
-      '<div id="lists">' +
-      '<ul><li>before</li></ul>' +
-      '<ul id="inline"><li>x</li></ul>' +
-      '<ul><li>after</li></ul>' +
-      '</div>'
-    );
-
-    document.body.appendChild(div);
-
-    EditorManager.init({
-      selector: '#inline',
-      inline: true,
-      add_unload_trigger: false,
-      skin: false,
-      plugins: 'lists',
-      disable_nodechange: true,
-      init_instance_callback(editor) {
-        Pipeline.async({}, Log.steps('TBA', 'Lists: Backspace delete inline tests', suite.toSteps(editor)), function () {
-          teardown(editor, div);
-          success();
-        }, failure);
-      },
-      valid_styles: {
-        '*': 'color,font-size,font-family,background-color,font-weight,font-style,text-decoration,float,' +
-        'margin,margin-top,margin-right,margin-bottom,margin-left,display,position,top,left,list-style-type'
-      }
+    Pipeline.async({}, [
+      Log.step('', 'Lists: Backspace at beginning of LI on body UL', Step.sync(() => {
+        editor.focus();
+        editor.selection.setCursorLocation(editor.getBody().firstChild.firstChild, 0);
+        editor.plugins.lists.backspaceDelete();
+        Assertions.assertEq('Assert number of ul', 3, DomQuery('#lists ul').length);
+        Assertions.assertEq('Assert number of li', 3, DomQuery('#lists li').length);
+      })),
+      Log.step('', 'Lists: Delete at end of LI on body UL', Step.sync(() => {
+        editor.focus();
+        editor.selection.setCursorLocation(editor.getBody().firstChild.firstChild, 1);
+        editor.plugins.lists.backspaceDelete(true);
+        Assertions.assertEq('Assert number of ul', 3, DomQuery('#lists ul').length);
+        Assertions.assertEq('Assert number of li', 3, DomQuery('#lists li').length);
+      }))
+    ], Element.fromHtml(), 
+    () => {
+      teardown(editor, div);
+      onSuccess();
+    }, 
+    (e) => {
+      teardown(editor, div);
+      onFailure(e);
     });
-  };
-
-  setup(success, failure);
+  }, {
+    inline: true,
+    add_unload_trigger: false,
+    skin: false,
+    plugins: 'lists',
+    disable_nodechange: true,
+    valid_styles: {
+      '*': 'color,font-size,font-family,background-color,font-weight,font-style,text-decoration,float,' +
+      'margin,margin-top,margin-right,margin-bottom,margin-left,display,position,top,left,list-style-type'
+    }
+  }, success, failure);
 });
